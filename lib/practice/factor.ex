dc0ecb5ae89c3cb375b107b4aeeaf081ef3bb3db
tina-n219 <noorani.t@husky.neu.edu>
2018-09-19 21:58:55 -0400

    isPrime buggy

--- lib/practice/factor.ex
+++ lib/practice/factor.ex
@@ -3,21 +3,22 @@ defmodule Practice.Factor do
         isPrime(x)
     end
 
-
     def isPrime(x) do
-       if x <= 1 do 
-        false 
-       end
+        # where i will try and create a for loop type function
+       isPrime(2,:math.sqrt(x), true, x)
+    end
 
-       i = 2
-       unless (i >= :math.sqrt(x)) do
-            if (rem(x, i) == 0) do
-                false  
-            else 
-                true
-            end
-            i = i + 1
-        end
-        
+    def isPrime(i, limit, youPrimeHuh, val) when i <= limit do
+        # recurse?
+        # recurse to a new i value, update if the value is still prime
+        # to be prime, val must always have a remainder when divided by i
+
+        isPrime(i + 1, limit, ((rem(val, i) == 0) && youPrimeHuh) , val)
+    end
+
+    # when you have finished with the for loop, return whether or not val was not 
+    # evenly divisable by any i
+    def isPrime(i, limit, youPrimeHuh, val) when i > limit do
+        youPrimeHuh
     end
 end
