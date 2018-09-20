5ee3fbff30d40081e7a184c72c2231169dc06cc8
tina-n219 <noorani.t@husky.neu.edu>
2018-09-19 22:14:13 -0400

    i did it

--- lib/practice/factor.ex
+++ lib/practice/factor.ex
@@ -9,13 +9,13 @@ defmodule Practice.Factor do
     end
 
     def isPrime(i, limit, youPrimeHuh, val) when i <= limit do
-        # recurse?
+        # recurse???
         # recurse to a new i value, update if the value is still prime
         # to be prime, val must always have a remainder when divided by i
-
-        isPrime(i + 1, limit, ((rem(val, i) == 0) && youPrimeHuh) , val)
+        isPrime(i + 1, limit, ((rem(val, i) != 0) && youPrimeHuh) , val)
     end
 
+    # Break case:
     # when you have finished with the for loop, return whether or not val was not 
     # evenly divisable by any i
     def isPrime(i, limit, youPrimeHuh, val) when i > limit do
